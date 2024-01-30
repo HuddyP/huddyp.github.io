66f6643409072360022b2e635f9f7e59b9b8c531
Hudson Pfenning <117323903+HuddyP@users.noreply.github.com>
2024-01-30 09:40:42 -0600

    reverted changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AboutMeComponent } from './src/app/webpages/about-me/about-me.component';
-import { MyProjectsComponent } from './src/app/webpages/my-projects/my-projects.component';
-import { ContactMeComponent } from './src/app/webpages/contact-me/contact-me.component';
+import { AboutMeComponent } from './webpages/about-me/about-me.component';
+import { MyProjectsComponent } from './webpages/my-projects/my-projects.component';
+import { ContactMeComponent } from './webpages/contact-me/contact-me.component';
 
 const routes: Routes = [
   { path: 'aboutme', component: AboutMeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeadshotComponent } from 'src/app/headshot/headshot.component';
-import { NavbarComponent } from 'src/app/navbar/navbar.component';
+import { HeadshotComponent } from './headshot/headshot.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { AboutMeComponent } from 'src/app/webpages/about-me/about-me.component';
-import { MyProjectsComponent } from 'src/app/webpages/my-projects/my-projects.component';
-import { ContactMeComponent } from 'src/app/webpages/contact-me/contact-me.component';
-import { FooterBarComponent } from 'src/app/footer-bar/footer-bar.component';
-import { AudioPlayerComponent } from 'src/app/audio-player/audio-player.component';
-import { AudioFabComponent } from 'src/app/audio-fab/audio-fab.component';
+import { AboutMeComponent } from './webpages/about-me/about-me.component';
+import { MyProjectsComponent } from './webpages/my-projects/my-projects.component';
+import { ContactMeComponent } from './webpages/contact-me/contact-me.component';
+import { FooterBarComponent } from './footer-bar/footer-bar.component';
+import { AudioPlayerComponent } from './audio-player/audio-player.component';
+import { AudioFabComponent } from './audio-fab/audio-fab.component';
 
 @NgModule({
   declarations: [
